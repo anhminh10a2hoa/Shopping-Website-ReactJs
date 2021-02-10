e60de29927e165d37a00effc840a70bf5c48f5d0
Minh Hoang <anhminh10a2hoa@gmail.com>
2021-02-11 01:36:08 +0200

    created our collection page and converted our shop items

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,13 @@
-import React, { Component } from 'react'
-import SHOP_DATA from './shop.data'
+import React from 'react';
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      collections: SHOP_DATA,
-    }
-  }
-
-  render() {
-    const {collections} = this.state;
-    return(
-      <div className="shop-page">
-        {collections.map(({id, ...otherCollectionsProps}) => (
-          <CollectionPreview key={id} {...otherCollectionsProps} />
-        ))}
-      </div>
-    )
-  }
+const ShopPage = () => {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  )
 }
 
 export default ShopPage;
