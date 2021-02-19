4a1865db7f1b7b5cf1617046e941af11cd4cc8a6
Minh Hoang <anhminh10a2hoa@gmail.com>
2021-02-19 19:13:25 +0200

    google sign in into saga

--- src/App.js
+++ src/App.js
@@ -10,32 +10,32 @@ import ShopPage from './pages/shop/shop.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
-  componentDidMount() {
-    const { setCurrentUser } = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth){
-        const userRef = await createUserProfileDocument(userAuth);
+  // componentDidMount() {
+  //   const { setCurrentUser } = this.props;
+  //   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+  //     if(userAuth){
+  //       const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          })
-        })
-      }
-      setCurrentUser(userAuth);
-      // addCollectionAndItems('collections', collectionArray.map(({title, items}) => ({title, items})));
-    })
-  }
+  //       userRef.onSnapshot(snapShot => {
+  //         setCurrentUser({
+  //           currentUser: {
+  //             id: snapShot.id,
+  //             ...snapShot.data()
+  //           }
+  //         })
+  //       })
+  //     }
+  //     setCurrentUser(userAuth);
+  //     // addCollectionAndItems('collections', collectionArray.map(({title, items}) => ({title, items})));
+  //   })
+  // }
 
   componentWillUnmount() {
     console.log("unmount")
